84d9b745f6a5f2316999558d50fbc5ba13a67e8c
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-02-28 15:07:11 -0600

    pruebas de comits

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("que caja quiere abrir");
+int opciones = int.Parse(Console.ReadLine());
+
+switch (opciones)
+{
+    case 1:
+        int i = 1;
+        while(i < 5)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+        break; 
+}
